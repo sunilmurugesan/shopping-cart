5af6ac35de05cd76a11fdbddafd3224196842b2d
Sunil Murugesan <sunil.murugesan@hmrc.gov.uk>
2023-07-31 17:28:33 +0100

    Wire-in lombok maven dependency

---
+++ src/main/java/com/deltacapita/shoppingcart/model/Item.java
@@ -0,0 +1,13 @@
+package com.deltacapita.shoppingcart.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Item {
+
+  private String name;
+
+}
